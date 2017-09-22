5019f718712418d45433043bb763b2c51cf34dec
Bruce Graham <brucegraham11@gmail.com>
2017-09-22 00:05:02 -0700

    Add component files

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+
+  }
+}
+
+ReactDOM.render('<App />', document.querySelector('#container'));

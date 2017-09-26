2b581e6bb71e8674327d95a4cb872b872163c0ac
Bruce <brucegraham11@gmail.com>
2017-09-26 14:47:50 -0700

    Add apiHelpers file

---
+++ server/apiHelpers.js
@@ -0,0 +1,25 @@
+const Axios = require('axios');
+const circular = require('circular');
+const config = require('./config.js');
+
+module.exports = {
+  getInstagramPhotos: callback => {
+    const num_photos = 18;
+
+    Axios.get('https://api.instagram.com/v1/users/' + config.userid + '/media/recent', {
+      params: {
+        access_token: config.token,
+        count: num_photos
+      }
+    })
+    .then(data => {
+      let json = JSON.stringify(data, circular());
+      let restoredObj = JSON.parse(json);
+
+      callback(restoredObj.data.data)
+    })
+    .catch(err => {
+      callback(err);
+    });
+  }
+};

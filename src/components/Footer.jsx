a7aa0d3c2dcda6159e059d0d1625021493e23b7e
Bruce <brucegraham11@gmail.com>
2017-09-26 14:57:48 -0700

    Add code to component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <section className="padding background-dark full-width">
+        <div className="s-12 l-6">
+          <p className="text-size-12">Copyright © 2017, Bruce Graham</p>
+        </div>
+        <div className="s-12 l-6">
+        </div>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;

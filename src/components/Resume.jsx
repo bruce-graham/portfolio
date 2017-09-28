5de82d38ccb285e03e33b7e879135f05b96acacc
Bruce <brucegraham11@gmail.com>
2017-09-27 23:02:09 -0700

    Now passing props down into the Header component

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
+import Header from './Header.jsx';
 
-const Resume = () => {
-  return (
-    <article>
-      <header className="section background-dark">
-        <div className="line">
-          <h1 className="text-white margin-top-bottom-40 text-size-60 text-line-height-1">
-            Resume
-          </h1>
-          <p className="margin-bottom-0 text-size-16">
-            Full Stack Developer
-          </p>
-        </div>
-      </header>
-      <div className="section background-white">
-        <div className="line">
-          <img src="img/resume.png" />
-        </div>
-      </div>
-    </article>
-  );
+class Resume extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      title: 'Resume',
+      description: 'Full Stack Developer'
+    };
+  };
+
+  render() {
+    return (
+      <main role="main">
+        <article>
+          <Header
+            title={this.state.title}
+            description={this.state.description}
+          />
+          <div className="section background-white">
+            <div className="line">
+              <img src="img/resume.png" />
+            </div>
+          </div>
+        </article>
+      </main>
+    );
+  };
 };
 
 export default Resume;

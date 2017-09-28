5c681654aae9d627063c967c6fa7337377e21586
Bruce <brucegraham11@gmail.com>
2017-09-27 23:06:28 -0700

    Header was broke out into an independent componenet to reduce boilerplate in all the components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({ title, description }) => {
   return (
-    <header role="banner" className="position-absolute">
-      <nav className="background-transparent background-transparent-hightlight full-width sticky">
-        <div className="s-12 l-2">
-          <div className="logo">
-            <img className="logo-white" src="img/LogoWhite.png" alt="" />
-            <img className="logo-dark" src="img/LogoDark.png" alt="" />
-          </div>
-        </div>
-        <div className="top-nav s-12 l-10">
-          <p className="nav-text"></p>
-          <ul className="right chevron">
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/portfolio">Portfolio</Link>
-            </li>
-            <li>
-              <Link to="/resume">Resume</Link>
-            </li>
-            <li>
-              <Link to="/gallery">Gallery</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+    <header className="section background-dark">
+      <div className="line">
+        <h1 className="text-white margin-top-bottom-40 text-size-60 text-line-height-1">
+          { title }
+        </h1>
+        <p className="margin-bottom-0 text-size-16">
+          { description }
+        </p>
+      </div>
     </header>
   );
 };

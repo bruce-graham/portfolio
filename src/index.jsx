71ed96191f1f9411e0d09e471a23aa4f8db40dba
Bruce <brucegraham11@gmail.com>
2017-09-27 22:59:27 -0700

    Removal of BrowserRouter from index.jsx into App.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch } from 'react-router-dom';
 import App from './components/App.jsx';
 
-ReactDOM.render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-), document.getElementById('container'));
+ReactDOM.render(<App />, document.getElementById('container'));
